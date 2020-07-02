b1a57fc706af8f3539e7f228183c8236ed3cfa9b
Jimmy <jimmy123good@hotmail.com>
2020-07-02 03:15:04 -0700

    Updates

---
+++ precache-manifest.1e5fa220b71f0e9d16c57f159a9ebe2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f47d2e3a3a281b5dd8afc914f6cde16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3757b6a476df1d7de908",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "eddba7e61cfaa528f683",
+    "url": "/static/css/main.2045b672.chunk.css"
+  },
+  {
+    "revision": "3757b6a476df1d7de908",
+    "url": "/static/js/2.1d03be5f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1d03be5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eddba7e61cfaa528f683",
+    "url": "/static/js/main.1197e496.chunk.js"
+  },
+  {
+    "revision": "77d643a401e085eab755",
+    "url": "/static/js/runtime-main.ad177ad1.js"
+  }
+]);
