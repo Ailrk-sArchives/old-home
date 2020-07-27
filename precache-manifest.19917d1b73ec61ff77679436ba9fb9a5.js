952d21e85185b1ce3ba3de4e5867f14b8f0ca3e2
Jimmy <jimmy123good@hotmail.com>
2020-07-26 17:55:10 -0700

    Updates

--- precache-manifest.19917d1b73ec61ff77679436ba9fb9a5.js
+++ precache-manifest.19917d1b73ec61ff77679436ba9fb9a5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f2aac17f190659c3331e80bd08012846",
+    "revision": "60e6f1009839c015e78c994defad6b8a",
     "url": "/home/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/css/2.d51e3ce7.chunk.css"
   },
   {
-    "revision": "5e79cf9b7eb0aebe0d01",
+    "revision": "38a4b1ee9d09889b3fcb",
     "url": "/home/static/css/main.2045b672.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.ea9461c5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5e79cf9b7eb0aebe0d01",
-    "url": "/home/static/js/main.cc9dd83e.chunk.js"
+    "revision": "38a4b1ee9d09889b3fcb",
+    "url": "/home/static/js/main.bb120c60.chunk.js"
   },
   {
     "revision": "ecd5d8de00ad578c65ef",
